7f2789eeea16826ad03e29bf17a419cb89357e45
Lesya <lesyazhornyk50@gmail.com>
2023-10-03 11:40:20 +0200

    new folder json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import Profile from '../Profile/Profile';
-import user from '../Profile/user.json';
-import Statistics from '../Statistics/Statistics';
-import data from '../Statistics/data.json';
-import FriendList from '../FriendList/FriendList';
-import friends from '../FriendList/friends.json'; 
-import TransactionHistory from '../TransactionHistory/TransactionHistory';
-import transactions from '../TransactionHistory/transactions.json'; 
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json'; 
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json'; 
 
 export const App = () => {
   return (
